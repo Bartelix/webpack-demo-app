a88555edd1754030d85ecf28dac7e250404767a1
Bartłomiej Bartoszewicz <bartlomiej.bartoszewicz95@gmail.com>
2022-03-19 11:54:59 +0100

    finish

--- webpack.common.js
+++ webpack.common.js
@@ -1,29 +1,13 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	entry: {
 		main: "./src/index.js",
 		vendor: "./src/vendor.js",
 	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./src/template.html",
-		}),
-	],
+
 	module: {
 		rules: [
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					// Creates `style` nodes from JS strings
-					"style-loader",
-					// Translates CSS into CommonJS
-					"css-loader",
-					// Compiles Sass to CSS
-					"sass-loader",
-				],
-			},
 			{
 				test: /\.html$/i,
 				loader: "html-loader",
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,8 @@ const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
 	mode: "production",
@@ -9,5 +11,33 @@ module.exports = merge(common, {
 		filename: "[name].[contenthash].bundle.js",
 		path: path.resolve(__dirname, "dist"),
 	},
-	plugins: [new CleanWebpackPlugin()],
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "[name].[contenthash].css",
+		}),
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: "./src/template.html",
+			minify: {
+				removeAttributeQuotes: true,
+				collapseWhitespace: true,
+				removeComments: true,
+			},
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Extract css info files
+					MiniCssExtractPlugin.loader,
+					// Translates CSS into CommonJS
+					"css-loader",
+					// Compiles Sass to CSS
+					"sass-loader",
+				],
+			},
+		],
+	},
 });
